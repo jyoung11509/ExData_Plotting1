9331914b0c6620919c07a5823289d824f8615557
jyoung11509 <jyoung11509@gmail.com>
2015-02-08 22:14:53 -0600

    This was finished on Feb 8, 2015 at 10:15 pm CST

---
+++ plot2.R
@@ -0,0 +1,6 @@
+data <- read.table("household_power_consumption.txt", sep=";", header=T, skip=66636, nrows=2879, col.names=c("Date","Time","Global_active_power","Global_reactive_power", "Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+data$datetime <- paste(data$Date, data$Time)
+data$datetime <- strptime(data$datetime, format="%d/%m/%Y %H:%M:%S")
+png(file="plot2.png")
+with(data,plot(datetime,Global_active_power,type="l",ylab="Global Active Power (kilowatts)",xlab=""))
+dev.off()
